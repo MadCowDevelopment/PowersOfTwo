444941bb348d2dd29953a28f8907b231eaf6ac6d
Mad <devdumbblind@googlemail.com>
2014-04-12 21:43:00 +0800

    replaced ProgressBar in Queue with automated playfield

--- PowersOfTwo/ViewModels/QueueViewModel.cs
+++ PowersOfTwo/ViewModels/QueueViewModel.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using System.Windows.Threading;
+using PowersOfTwo.Core;
 using PowersOfTwo.Framework;
 using PowersOfTwo.Services;
 
@@ -9,12 +14,62 @@ namespace PowersOfTwo.ViewModels
         private readonly MainWindowViewModel _mainWindowViewModel;
         private readonly GameProxy _gameProxy;
 
+        private GameLogic _gameLogic;
+
+        private readonly DispatcherTimer _timer;
+
         public QueueViewModel(MainWindowViewModel mainWindowViewModel, GameProxy gameProxy)
         {
             _mainWindowViewModel = mainWindowViewModel;
             _gameProxy = gameProxy;
+
+            InitializeGameLogic();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(100);
+            _timer.Tick += TimerTick;
+            _timer.Start();
+
             LeaveQueueCommand = new RelayCommand(p => LeaveQueue());
         }
+
+        private void InitializeGameLogic()
+        {
+            _gameLogic = new GameLogic(4, 4);
+            _gameLogic.OutOfMoves += GameLogicOutOfMoves;
+            Cells = _gameLogic.Cells;
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            var randomDirection = RNG.Next(0, 4);
+            switch (randomDirection)
+            {
+                case 0:
+                    _gameLogic.MoveLeft();
+                    break;
+                case 1:
+                    _gameLogic.MoveRight();
+                    break;
+                case 2:
+                    _gameLogic.MoveUp();
+                    break;
+                case 3:
+                    _gameLogic.MoveDown();
+                    break;
+            }
+
+            Cells = _gameLogic.Cells.ToList();
+        }
+
+        private void GameLogicOutOfMoves()
+        {
+            _timer.Stop();
+            InitializeGameLogic();
+            _timer.Start();
+        }
+
+        public List<NumberCell> Cells { get; private set; }
 
         private void LeaveQueue()
         {
